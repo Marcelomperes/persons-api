c49775078384c0fae1da3a476971d093022f14c2
Marcelo Martins <mmartinsperes@hotmail.com>
2021-10-29 18:22:45 -0300

    Developed delete by id operation

--- src/main/java/mmartins/personapi/service/PersonService.java
+++ src/main/java/mmartins/personapi/service/PersonService.java
@@ -44,8 +44,19 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-       Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExistis(id);
         return personMapper.toDTO(person);
     }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        Person person = verifyIfExistis(id);
+
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExistis(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
 }
